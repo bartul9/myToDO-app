d9e837cfb32593a335e48a77ab9a0b30fd98424f
bartul_9 <bartul123@outlook.com>
2021-01-11 20:13:42 +0100

    Update app.js

--- app.js
+++ app.js
@@ -4,24 +4,75 @@ const addNew = document.querySelector(".addNewBtn");
 
 const ul = document.querySelector(".myList");
 
-const liElement = document.querySelector(".clickNew");
-
 const myInput = document.getElementById("myInput");
 
-const deleteBtn = document.getElementsByClassName("delete");
+// Todo array
+
+const myTodo = ["Go Shopping", "Learn how to code"];
+
+//Create delete btn
+let delBtn = document.createElement("button");
+delBtn.type = "button";
+delBtn.innerHTML = "Del";
+delBtn.className = "btn btn-warning delete del";
 
 // Function to add element on click
 
 addNew.addEventListener("click", function () {
-  const li = document.createElement("li");
+  //Create text node from input
   const textN = document.createTextNode(myInput.value);
+
+  if (textN.textContent === "") {
+    alert("Please enter some text");
+    return;
+  }
+
+  //Push value of input to array
+  const input = document.querySelector("#myInput").value;
+  myTodo.push(input);
+
+  let delBtn = document.createElement("button");
+  delBtn.type = "button";
+  delBtn.innerHTML = "Del";
+  delBtn.className = "btn btn-warning delete del";
+
+  //Create li element on click and append it to ul, with del btn
+  const li = document.createElement("li");
+  li.className = "clickNew var";
   li.appendChild(textN);
+  li.appendChild(delBtn);
   ul.appendChild(li);
+
+  // Delete each element on del btn click
+
+  const all = document.getElementsByClassName("btn btn-warning delete del");
+
+  for (let i = 0; i < all.length; i++) {
+    all[i].onclick = function () {
+      let div = this.parentElement;
+      div.style.display = "none";
+    };
+  }
 });
 
-//Delete element function
+//
 
-deleteBtn.addEventListener("click", function () {
-  liElement.classList.add("displayNone");
-  console.log("hello");
-});
+//Function for displaying messages in array
+
+//const myList = document.querySelector(".myList");
+
+// const displayMessages = function (x) {
+//   x.forEach((y, i) =>
+//     myList.insertAdjacentHTML(
+//       "beforeend",
+//       ` <li class="clickNew var-${i}">
+//   <span>${y}</span>
+//   <button type="button" class="btn btn-warning delete del-">Del</button>
+// </li>`
+//     )
+//   );
+// };
+
+// displayMessages(myTodo);
+
+// var arr = [12, 10, 8, 12, 7, 6, 4, 10, 12];
